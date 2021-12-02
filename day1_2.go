8dd1a68a67c134e580cf5f35257b623969148338
Jon Nelson <phelix08@gmail.com>
2021-12-01 20:05:28 -0600

    day1 - buffers are easier

--- day1_2.go
+++ day1_2.go
@@ -16,25 +16,46 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	windowSize := 3
-	window := make([]int, windowSize)
-	previousSum := 0
+	buffer := NewBuffer(windowSize)
 	transitions := 0
 
 	for i := 0; scanner.Scan(); i++ {
 		value, _ := strconv.Atoi(scanner.Text())
-		window[i%windowSize] = value
 
-		currentSum := 0
-		if (i+1) >= windowSize {
-			for j := 0; j < windowSize; j++ {
-				currentSum += window[(i-j) % windowSize]
-			}
-		}
+		previousSum := buffer.Sum()
+		buffer.Push(value)
 
-		if i >= windowSize && currentSum > previousSum {
+		if i >= windowSize && buffer.Sum() > previousSum {
 			transitions++
 		}
-		previousSum = currentSum
 	}
 	log.Printf("transitions: %d", transitions)
+}
+
+
+type Buffer struct {
+	buffer []int
+	size int
+	capacity int
+}
+
+func NewBuffer(capacity int) *Buffer {
+	return &Buffer{
+		buffer: make([]int, capacity),
+		size: 0,
+		capacity: capacity,
+	}
+}
+
+func (b *Buffer) Push(i int) {
+	b.buffer[b.size % b.capacity] = i
+	b.size++
+}
+
+func (b Buffer) Sum() int {
+	sum := 0
+	for i := 0; i < b.size && i < b.capacity; i++ {
+		sum += b.buffer[i]
+	}
+	return sum
 }
